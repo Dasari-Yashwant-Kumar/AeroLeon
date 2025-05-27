e2ecb2d6ec74594c1fbcc57a8d3b6bd098b0c4eb
Dasari-Yashwant-Kumar <dasariyashwantk@gmail.com>
2025-05-28 01:48:01 +0530

    Added some new features

--- flight.js
+++ flight.js
@@ -1,23 +1,23 @@
 let airportData = {};
 
-const airport = async ()=>{
-    let response = await fetch ("https://raw.githubusercontent.com/mwgg/Airports/master/airports.json");
-        airportData = await response.json();
+const airport = async () => {
+    let response = await fetch("https://raw.githubusercontent.com/mwgg/Airports/master/airports.json");
+    airportData = await response.json();
 }
 
 airport();
 
-const filterAirport = (input) =>{
+const filterAirport = (input) => {
     let result = [];
     let inputintolower = input.toLowerCase();
 
-    for (const code in airportData){
+    for (const code in airportData) {
         const airport = airportData[code];
         const name = airport.name && airport.name.toLowerCase() || "";
         const city = airport.city && airport.city.toLowerCase() || "";
         const country = airport.country && airport.country.toLowerCase() || "";
 
-        if (name.includes(inputintolower) || city.includes(inputintolower) || country.includes(inputintolower)){
+        if (name.includes(inputintolower) || city.includes(inputintolower) || country.includes(inputintolower)) {
             result.push(`${airport.name} (${airport.city}, ${airport.country})`)
         }
     }
@@ -25,28 +25,27 @@ const filterAirport = (input) =>{
     return result;
 }
 
-function showResult(result,inputElement){
+function showResult(result, inputElement) {
     let resultEl = inputElement.closest(".input-wrapper").querySelector(".result");
-
     resultEl.innerHTML = "";
 
-    if(result.length === 0){
+    if (result.length === 0) {
         result.innerHTML = "<li>No Result Found</li>";
         return;
     }
 
-    result.forEach(item=>{
+    result.forEach(item => {
         const li = document.createElement("li");
         li.innerText = item;
 
-        li.addEventListener("click", ()=>{
+        li.addEventListener("click", () => {
             inputElement.value = item;
             resultEl.innerHTML = "";
         })
 
 
         resultEl.appendChild(li);
-        
+
 
     })
 }
@@ -56,10 +55,10 @@ const toInput = document.getElementById("to");
 
 
 
-const handleInput= (event)=>{
+const handleInput = (event) => {
     const input = event.target
     const value = input.value.trim();
-    if(value.length > 1){
+    if (value.length > 1) {
         const filtered = filterAirport(value);
         showResult(filtered.slice(0, 10), input)
     } else {
@@ -69,6 +68,59 @@ const handleInput= (event)=>{
 
 fromInput.addEventListener("input", handleInput);
 toInput.addEventListener("input", handleInput);
+
+const seats = document.querySelector("#seats");
+
+const seatCount = () => {
+    const value = seats.value.trim();
+
+    if (value == "") {
+        return
+    }
+    const totalSeats = Number(value);
+    if (totalSeats > 10) {
+        alert("The maximum member allowed are 10");
+        seats.value = "";
+    } else if (totalSeats === 0) {
+        alert("Select atleast 1 seat");
+        seats.value = "";
+    }
+
+}
+
+seats.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        seatCount();
+        seats.blur();
+    }
+
+}
+)
+
+seats.addEventListener("blur", () => {
+    seatCount();
+});
+
+const search = document.querySelector(".search");
+const flightSection = document.querySelector(".flight-section");
+const bottom = document.querySelector(".bottom");
+const departure = document.querySelector("#departure");
+const returnBack = document.querySelector("#return");
+
+search.addEventListener("click", ()=>{
+    if(fromInput.value !== "" && toInput.value !== "" && seats.value !== "" && departure.value !== "" && returnBack.value !== ""){
+        flightSection.classList.toggle("selected");
+    bottom.classList.toggle("chosen");
+    flightSection.scrollIntoView({behavior: "smooth", block: "center"})
+    } else{
+        alert("Enter all the information");
+    }
+
+
+    
+})
+
 
 
 
