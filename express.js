0db4ecd81a2fe709e687d62f087bf473c9be7640
Dasari-Yashwant-Kumar <dasariyashwantk@gmail.com>
2025-06-15 13:15:26 +0530

    updated

--- express.js
+++ express.js
@@ -38,16 +38,6 @@ const getAccessToken = async () => {
   }
 };
 
-const path = require("path");
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-
 app.get('/api/flights', async (req, res) => {
   console.log("🛫 /api/flights called");
   console.log("Query params:", req.query);
