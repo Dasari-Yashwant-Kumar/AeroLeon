7716576fabb0b544ab8c55065990fe1aee6bc4ef
Dasari-Yashwant-Kumar <dasariyashwantk@gmail.com>
2025-06-20 12:05:55 +0530

    updated the json

--- backend/express.js
+++ backend/express.js
@@ -1,3 +1,5 @@
+require('dotenv').config({ path: './backend/.env' });
+
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
@@ -98,4 +100,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
 
