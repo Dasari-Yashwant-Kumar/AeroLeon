bb4dff60dc4e02dbb9b3277efaa4d43c1e41be35
Dasari-Yashwant-Kumar <dasariyashwantk@gmail.com>
2025-10-01 14:02:17 +0530

    updated the responsiveness

--- frontend/src/Intro.jsx
+++ frontend/src/Intro.jsx
@@ -15,21 +15,21 @@ const Landingpage = () => {
 
                 <div className="">
 
-                    <h1 className="absolute top-[2rem] left-[50%] -translate-x-[50%] text-[#262D2D]">AeroLeon</h1>
+                    <h1 className="absolute top-[2rem] left-[50%] -translate-x-[50%] text-[1.6rem] text-[#262D2D]">AeroLeon</h1>
 
-                    <div className="text-center text-[#FFFFFF]">
+                    <div className="absolute top-[5rem] flex flex-col items-center text-center px-4 gap-[15rem] text-[0.8rem] text-[#FFFFFF]">
 
-                        <div className="text-[0.8rem]">
+                        <div>
 
-                            <h1 className="absolute top-[4rem] left-[50%] -translate-x-[50%] font-sora">Explore the World</h1>
+                            <h1 className="pb-4 font-sora">Explore the World</h1>
 
-                            <p className="absolute top-[6rem] left-[50%] -translate-x-[50%] font-sora">Take off to new destinations with AeroLeon. Book
+                            <p className="font-sora">Take off to new destinations with AeroLeon. Book
                                 flights easily and start your next adventure without hassle.
                                 The world is waiting—let’s explore it together!
                             </p>
                         </div>
                         <Link to="/FlightSearch">
-                            <div className="absolute top-[30rem] left-[50%] -translate-x-[50%] text-[0.8rem] gap-2 flex items-center justify-center text-center pt-[2rem] md:text-[1rem]
+                            <div className="gap-2 flex items-center justify-center text-center pt-[2rem] md:text-[1rem]
                          font-sora cursor-pointer" id="adventure">
                                 <p>Start My Adventure</p>
                                 <FontAwesomeIcon icon={faArrowRight} className='lg:mt-2.5 lg:!w-[2.5rem] lg:!h-[2.5rem]' />
