a3d30bf3e5f743057b77266ff0ce7602009cc03b
Dasari-Yashwant-Kumar <dasariyashwantk@gmail.com>
2025-10-01 08:15:49 +0530

    min h

--- frontend/src/Intro.jsx
+++ frontend/src/Intro.jsx
@@ -6,8 +6,8 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Landingpage = () => {
     return (
-        <div className="p-0 m-0 bg-[#262D2D] overflow-y-hidden font-sans">
-            <div className = "relative flex justify-center items-center  min-h-screen">
+        <div className="p-0 m-0 bg-[#262D2D] overflow-y-hidden  min-h-screen font-sans">
+            <div className = "relative flex justify-center items-center ">
             <img className="p-[15%] w-full max-w-[1500px] h-auto rounded-[55px] " src={plane1} alt="plane" />
 
             <div className=" absolute lg:top-[25%] lg:left-[10%] 2xl:left-[15%] ">
