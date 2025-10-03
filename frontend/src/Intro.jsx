83c4d2b1c4974d20d8951ad80898de6699cdce45
Dasari-Yashwant-Kumar <dasariyashwantk@gmail.com>
2025-10-03 13:46:21 +0530

    The final change

--- frontend/src/Intro.jsx
+++ frontend/src/Intro.jsx
@@ -14,13 +14,13 @@ const Landingpage = () => {
                 <img className=" w-[80vw] h-[80vh] max-w-[2500px] max-h-[1500px] rounded-[50px]" src={plane1} alt="plane" />
 
               <div className="absolute top-[2rem] flex flex-col justify-center items-center text-center px-4 gap-[2rem] 
-    text-[0.8rem] md:top-[5rem] md:items-start md:text-left md:gap-[2.7rem] md:pl-[2rem] md:pr-[22rem] lg:pl-[2rem] lg:pr-[29rem] xl:pr-[33rem]">
+    text-[0.8rem] md:top-[4rem] md:items-start md:text-left md:gap-[2.5rem] md:pl-[2rem] md:pr-[22rem] lg:pl-[2rem] lg:pr-[29rem] xl:pr-[33rem]">
 
-                    <h1 className="text-[1.6rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3rem]">AeroLeon</h1>
+                    <h1 className="text-[1.8rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[2.8rem]">AeroLeon</h1>
 
                     <div className="md:flex md:flex-col md:items-start md:text-left md:gap-[3rem] lg:gap-[2rem] text-[#FFFFFF]">
                         <div>
-                            <h1 className="pb-4 text-[1.6rem] sm:text-[1.8rem] md:text-[2.2rem] lg:text-[2.5rem]">Explore the World</h1>
+                            <h1 className="pb-4 text-[1.6rem] sm:text-[1.8rem] md:text-[2.2rem] lg:text-[2.4rem]">Explore the World</h1>
                             <p className="sm:text-[1rem] md:text-[1.2rem] lg:text-[1.3rem]">Take off to new destinations with AeroLeon. Book
                                 flights easily and start your next adventure without hassle.
                                 The world is waiting—let’s explore it together!
