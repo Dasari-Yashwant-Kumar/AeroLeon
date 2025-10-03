828f24369c7a8110ee4f3677a8ecb651cc421458
Dasari-Yashwant-Kumar <dasariyashwantk@gmail.com>
2025-10-03 21:45:47 +0530

    changes

--- frontend/src/Intro.jsx
+++ frontend/src/Intro.jsx
@@ -14,23 +14,23 @@ const Landingpage = () => {
                 <img className=" w-[80vw] h-[80vh] max-w-[2500px] max-h-[1500px] rounded-[50px]" src={plane1} alt="plane" />
 
               <div className="absolute top-[2rem] flex flex-col justify-center items-center text-center px-4 gap-[2rem] 
-    text-[0.8rem] md:top-[4rem] md:items-start md:text-left md:gap-[2.5rem] md:pl-[2rem] md:pr-[22rem] lg:pl-[2rem] lg:pr-[29rem] xl:top-[5rem] xl:pr-[33rem]">
+    text-[0.8rem] md:top-[4rem] md:items-start md:text-left md:gap-[2.5rem] md:pl-[2rem] md:pr-[22rem] lg:pr-[29rem] xl:top-[5rem] xl:pr-[33rem] 2xl:top-[10rem] 2xl:pr-[75rem] 2xl:gap-[8rem]">
 
-                    <h1 className="text-[1.8rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[2.8rem]">AeroLeon</h1>
+                    <h1 className="text-[1.8rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[2.8rem] 2xl:text-[5rem]">AeroLeon</h1>
 
-                    <div className="md:flex md:flex-col md:items-start md:text-left md:gap-[3rem] lg:gap-[2rem] text-[#FFFFFF]">
+                    <div className="md:flex md:flex-col md:items-start md:text-left md:gap-[3rem] lg:gap-[2rem] 2xl:gap-[10rem] text-[#FFFFFF]">
                         <div>
-                            <h1 className="pb-4 text-[1.6rem] sm:text-[1.8rem] md:text-[2.2rem] lg:text-[2.4rem]">Explore the World</h1>
-                            <p className="sm:text-[1rem] md:text-[1.2rem] lg:text-[1.3rem]">Take off to new destinations with AeroLeon. Book
+                            <h1 className="pb-4 text-[1.6rem] sm:text-[1.8rem] md:text-[2.2rem] lg:text-[2.4rem] 2xl:text-[4rem] ">Explore the World</h1>
+                            <p className="sm:text-[1rem] md:text-[1.2rem] lg:text-[1.3rem] 2xl:text-[3rem]">Take off to new destinations with AeroLeon. Book
                                 flights easily and start your next adventure without hassle.
                                 The world is waiting—let’s explore it together!
                             </p>
                         </div>
                         <Link to="/FlightSearch">
                             <div className="gap-2 flex items-center justify-center text-center text-[1rem] pt-[18rem] md:pt-[0] sm:text-[1rem] md:text-[1.2rem] lg:text-[1.3rem]
-                        cursor-pointer" id="adventure">
+                         2xl:text-[3rem] cursor-pointer" id="adventure">
                                 <p>Start My Adventure</p>
-                                <FontAwesomeIcon icon={faArrowRight} className="sm:text-[1rem] md:text-[1.2rem] lg:text-[1.3rem]" />
+                                <FontAwesomeIcon icon={faArrowRight} className="sm:text-[1rem] md:text-[1.2rem] lg:text-[1.3rem]  2xl:text-[3rem]" />
                             </div>
                         </Link>
                     </div>
