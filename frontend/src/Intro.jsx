9dba5c3c8edcfd0d1c563ee1b4abbd6226e1ae26
Dasari-Yashwant-Kumar <dasariyashwantk@gmail.com>
2025-10-03 13:23:04 +0530

    udpated the intro

--- frontend/src/Intro.jsx
+++ frontend/src/Intro.jsx
@@ -14,14 +14,14 @@ const Landingpage = () => {
                 <img className=" w-[75vw] h-[80vh] max-w-[2500px] max-h-[1500px] rounded-[50px]" src={plane1} alt="plane" />
 
               <div className="absolute top-[2rem] flex flex-col justify-center items-center text-center px-4 gap-[2rem] 
-    text-[0.8rem] md:top-[5rem] md:items-start md:text-left md:gap-[4rem] md:pr-[18rem] lg:pl-[2rem] lg:pr-[28rem]">
+    text-[0.8rem] md:top-[5rem] md:items-start md:text-left md:gap-[3rem] md:pr-[18rem] lg:pl-[2rem] lg:pr-[27rem] xl:pr-[32rem]">
 
                     <h1 className="text-[1.6rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3rem]">AeroLeon</h1>
 
                     <div className="md:flex md:flex-col md:items-start md:text-left md:gap-[5rem] text-[#FFFFFF]">
                         <div>
                             <h1 className="pb-4 sm:text-[1.8rem] md:text-[2.2rem] lg:text-[2.5rem]">Explore the World</h1>
-                            <p className="sm:text-[1rem] md:text-[1.2rem] lg:text-[1.5rem]">Take off to new destinations with AeroLeon. Book
+                            <p className="sm:text-[1rem] md:text-[1.2rem] lg:text-[1.4rem]">Take off to new destinations with AeroLeon. Book
                                 flights easily and start your next adventure without hassle.
                                 The world is waiting—let’s explore it together!
                             </p>
@@ -30,7 +30,7 @@ const Landingpage = () => {
                             <div className="gap-2 flex items-center justify-center text-center text-[1rem] pt-[18rem] md:pt-[0] sm:text-[1rem] md:text-[1.2rem] lg:text-[1.5rem]
                         cursor-pointer" id="adventure">
                                 <p>Start My Adventure</p>
-                                <FontAwesomeIcon icon={faArrowRight} className="sm:text-[1rem] md:text-[1.2rem] lg:text-[1.5rem]]" />
+                                <FontAwesomeIcon icon={faArrowRight} className="sm:text-[1rem] md:text-[1.2rem] lg:text-[1.4rem]]" />
                             </div>
                         </Link>
                     </div>
