7fedd716df49cc5cf0fff5a457bf6712a6e9685a
Dasari-Yashwant-Kumar <dasariyashwantk@gmail.com>
2025-10-03 13:26:54 +0530

    updated the intro again

--- frontend/src/Intro.jsx
+++ frontend/src/Intro.jsx
@@ -14,11 +14,11 @@ const Landingpage = () => {
                 <img className=" w-[75vw] h-[80vh] max-w-[2500px] max-h-[1500px] rounded-[50px]" src={plane1} alt="plane" />
 
               <div className="absolute top-[2rem] flex flex-col justify-center items-center text-center px-4 gap-[2rem] 
-    text-[0.8rem] md:top-[5rem] md:items-start md:text-left md:gap-[3rem] md:pr-[18rem] lg:pl-[2rem] lg:pr-[27rem] xl:pr-[32rem]">
+    text-[0.8rem] md:top-[5rem] md:items-start md:text-left md:gap-[2.7rem] md:pr-[18rem] lg:pl-[2rem] lg:pr-[27rem] xl:pr-[32rem]">
 
                     <h1 className="text-[1.6rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3rem]">AeroLeon</h1>
 
-                    <div className="md:flex md:flex-col md:items-start md:text-left md:gap-[5rem] text-[#FFFFFF]">
+                    <div className="md:flex md:flex-col md:items-start md:text-left md:gap-[3rem] lg:gap-[2rem] text-[#FFFFFF]">
                         <div>
                             <h1 className="pb-4 sm:text-[1.8rem] md:text-[2.2rem] lg:text-[2.5rem]">Explore the World</h1>
                             <p className="sm:text-[1rem] md:text-[1.2rem] lg:text-[1.4rem]">Take off to new destinations with AeroLeon. Book
